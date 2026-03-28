f6146cd2762cbab1b3a073bbbfaba1b66ad40eff
Matthew Wen <you@example.com>
2026-03-28 18:13:42 -0400

    switched to env variables, global commands

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-const { ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
-const fs = require('node:fs');
-const path = require('node:path');
-const { Client, Collection, GatewayIntentBits } = require('discord.js');
-const { token } = require('./config.json');
-const { loadTrackedSubreddits, saveTrackedSubreddits } = require('./utils/dataUtils');
-const { fetchPosts } = require('./utils/redditAPI');
-const NotificationPoller = require('./utils/notificationPoller');
+const { ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
+const fs = require("node:fs");
+const path = require("node:path");
+const { Client, Collection, GatewayIntentBits } = require("discord.js");
+const { token } = process.env.DISCORD_TOKEN;
+const {
+  loadTrackedSubreddits,
+  saveTrackedSubreddits,
+} = require("./utils/dataUtils");
+const { fetchPosts } = require("./utils/redditAPI");
+const NotificationPoller = require("./utils/notificationPoller");
 
 // Initialize client with intents
 const client = new Client({
@@ -13,33 +16,37 @@ const client = new Client({
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.MessageContent,
-    GatewayIntentBits.GuildMessageReactions
-  ]
+    GatewayIntentBits.GuildMessageReactions,
+  ],
 });
 
 // Command collection
 client.commands = new Collection();
 
 // Load commands
-const commandsPath = path.join(__dirname, 'commands');
+const commandsPath = path.join(__dirname, "commands");
 const commandFolders = fs.readdirSync(commandsPath);
 
 for (const folder of commandFolders) {
   const folderPath = path.join(commandsPath, folder);
-  const commandFiles = fs.readdirSync(folderPath).filter(file => file.endsWith('.js'));
-  
+  const commandFiles = fs
+    .readdirSync(folderPath)
+    .filter((file) => file.endsWith(".js"));
+
   for (const file of commandFiles) {
     const filePath = path.join(folderPath, file);
     const command = require(filePath);
-    if ('data' in command && 'execute' in command) {
+    if ("data" in command && "execute" in command) {
       client.commands.set(command.data.name, command);
     }
   }
 }
 
 // Load events
-const eventsPath = path.join(__dirname, 'events');
-const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+const eventsPath = path.join(__dirname, "events");
+const eventFiles = fs
+  .readdirSync(eventsPath)
+  .filter((file) => file.endsWith(".js"));
 
 for (const file of eventFiles) {
   const filePath = path.join(eventsPath, file);
@@ -55,16 +62,16 @@ for (const file of eventFiles) {
 client.notificationPoller = null;
 
 // Handle process termination gracefully
-process.on('SIGINT', () => {
-  console.log('Received SIGINT, stopping notification poller...');
+process.on("SIGINT", () => {
+  console.log("Received SIGINT, stopping notification poller...");
   if (client.notificationPoller) {
     client.notificationPoller.stop();
   }
   process.exit(0);
 });
 
-process.on('SIGTERM', () => {
-  console.log('Received SIGTERM, stopping notification poller...');
+process.on("SIGTERM", () => {
+  console.log("Received SIGTERM, stopping notification poller...");
   if (client.notificationPoller) {
     client.notificationPoller.stop();
   }
