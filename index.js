c786eae9459c36f0e1ba5692f5bd6639abf999c5
Matthew Wen <you@example.com>
2026-03-28 18:16:06 -0400

    wrong type

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
 const fs = require("node:fs");
 const path = require("node:path");
 const { Client, Collection, GatewayIntentBits } = require("discord.js");
-const { token } = process.env.DISCORD_TOKEN;
+const token = process.env.DISCORD_TOKEN;
 const {
   loadTrackedSubreddits,
   saveTrackedSubreddits,
